9eae46ae7a0048d0cdbefe02daeea9fd440879ff
SriKynanti <kynanti.ti@gmail.com>
2022-02-14 14:08:46 +0700

    first commit

---
+++ Week1-Praktikum1/destructuring.js
@@ -0,0 +1,8 @@
+let polStudent = ({name, polytechnic}) => {
+    alert(`${name} from ${polytechnic}`);
+};
+
+polStudent({
+    name: 'Sri Kynanti',
+    polytechnic: 'Politeknik Negeri Malang'
+});
---
+++ Week1-Praktikum1/template.js
@@ -0,0 +1,13 @@
+let fname = 'Sri';
+let lname = 'Kynanti';
+let age = prompt("Masukkan umur Sri Kynanti!");
+
+// Memakai template strings
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
+
+
+
+
+
+
